6846600de271907c9d88386b99b0de36f866d514
Tushar Arora <ta@tagglabs.in>
2020-09-21 15:43:19 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { useAuth0 } from '@auth0/auth0-react';
+
+import Login from './Components/DepLogin/Login'
+import  DefaultLogin  from './Components/AuthLogin/DefaultLogin';
 
 function App() {
+
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        
+        <Route exact path='/simpleLogin'>
+          <Login />
+        </Route>
+
+        <Route  path='/'>
+          <DefaultLogin />  
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
   );
+  
+ 
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Auth0Provider } from "@auth0/auth0-react";
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={'dev-7sjka452.us.auth0.com'}
+    clientId={'H8XDPdWaatnVMCqYgtPvmQ2yU6B8q90W'}
+    redirectUri={window.location.origin}>
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
